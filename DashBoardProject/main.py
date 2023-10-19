a790d8f9c28ba6ee72ab60e69324012d5b85373d
CieranW <cieranwong@gmail.com>
2023-10-18 20:22:22 -0700

    Fixed the output so now it displays the name of the .csv file next to the values contained.
    Signed-off-by: CieranW <cieranwong@gmail.com>

--- DashBoardProject/main.py
+++ DashBoardProject/main.py
@@ -23,27 +23,27 @@ def read_csv(filename):
     return data
 
 
-def find_variable_locations(data_dict):
-    # Initialize a dictionary to store variable locations
-    variable_locations = {}
+def find_variable_locations_with_values(data_dict):
+    # Initialize a dictionary to store variable locations and values
+    variable_data = {}
 
     # Iterate through each file's data to collect unique variable names
     unique_variables = set()
     for data in data_dict.values():
         unique_variables.update(data.keys())
 
-    # Initialize the variable locations dictionary with the unique variable names
+    # Initialize the variable data dictionary with the unique variable names
     for variable in unique_variables:
-        variable_locations[variable] = []
-        print(variable)
+        variable_data[variable] = {"locations": [], "values": []}
 
-    # Append the file names to each respective variable
+    # Append the file names to each respective variable and store their values
     for filename, data in data_dict.items():
         for variable in unique_variables:
             if variable in data:
-                variable_locations[variable].append(filename)
+                variable_data[variable]["locations"].append(filename)
+                variable_data[variable]["values"].append(data[variable])
 
-    return variable_locations
+    return variable_data
 
 
 def print_data(data):
@@ -70,10 +70,14 @@ def main():
 
     # Now you have data from all CSV files in data_dict for comparison or future use
     # For example, you can access data from a specific file using data_dict["filename.csv"]
-    variable_locations = find_variable_locations(data_dict)
-    print("\nVariable Locations:")
-    for var, files in variable_locations.items():
-        print(f"{var} is found in: {', '.join(files)}")
+
+    variable_data = find_variable_locations_with_values(data_dict)
+    for var, data in variable_data.items():
+        locations = ", ".join(data["locations"])
+        print(f"\n{var} is found in: {locations}")
+        for i, location in enumerate(data["locations"]):
+            values = ", ".join(map(str, data["values"][i]))  # Convert values to strings
+            print(f"{var} in {location}: {values}")
 
 
 if __name__ == "__main__":
